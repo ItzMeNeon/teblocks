99ad72a777c420dedd896a5567ab1b4714f22517
ItzShino <143888260+ItzMeNeon@users.noreply.github.com>
2026-08-02 13:37:31 +0800

    new website look

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "TeBlocks";
+export const SITE_DESCRIPTION = "A competitive block-stacking game. Server-authoritative, SRS rotation, 7-bag, ranked and quick play.";
