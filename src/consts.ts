3a15f260159e84d4215143b840ffe2c1f58b4981
ItzShino <143888260+ItzMeNeon@users.noreply.github.com>
2026-08-25 16:48:13 +0800

    Changing some items as rebasing to TechMino

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "TeBlocks";
-export const SITE_DESCRIPTION = "A competitive block-stacking game. Server-authoritative, SRS rotation, 7-bag, ranked and quick play.";
+export const SITE_DESCRIPTION = "A competitive block-stacking game.";
