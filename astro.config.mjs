c375d409b4b0153d2cb395179e4f2cd08d5f86dc
ItzShino <143888260+ItzMeNeon@users.noreply.github.com>
2026-09-08 17:22:13 +0800

    missing config option on config.astro.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://teblocks.my.id",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	trailingSlash: "ignore",
+  site: "https://teblocks.my.id",
+  output: "server",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  trailingSlash: "ignore",
 });
